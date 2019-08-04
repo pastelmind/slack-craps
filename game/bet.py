48777f5e28df5645ee9342fd5602ad74040b87ae
pastelmind <keepyourhonor@gmail.com>
2019-08-04 17:58:14 +0900

    Redefine Bet and create subclasses
    Bet subclasses no longer store the type and bet amount (wager) as
    instance variables. Instead, the type is set as a class variable.
    The wager should be stored in a dictionary instead.
    
    Each subclass of Bet defines the check() method, which is used to
    determine the outcome of a bet after each roll.
    Also add a BetOutcome enum to represent each possible outcome.

--- game/bet.py
+++ game/bet.py
@@ -1,26 +1,116 @@
 """Provides classes for bets and bet types."""
 
 from enum import Enum, unique
+from typing import Union
 
 
 @unique
 class BetType(Enum):
-    """Represents a type of bet."""
+    """A type of bet."""
     PASS = 'pass'
     DONT_PASS = 'dont_pass'
     PASS_ODDS = 'pass_odds'
     DONT_PASS_ODDS = 'dont_pass_odds'
 
 
-# TODO Actually make this useful, or remove it
+@unique
+class BetOutcome(Enum):
+    """Represents the outcome of a bet."""
+    UNDECIDED = 0
+    WIN = 1
+    LOSE = 2
+    TIE = 3
+
+
 class Bet:
-    """Represents a single bet.
+    """A single bet.
 
     Args:
         bet_type: The type of the bet.
         amount: Amount to bet.
+
+    Attributes:
+        type: String representing the type of the bet.
     """
 
-    def __init__(self, bet_type: BetType, amount: int) -> None:
-        self.type = bet_type
-        self.amount = amount
+    type: BetType = NotImplementedError('Must be overridden in a child class')
+
+    def check(self, *, roll: int, point: Union[int, None]) -> BetOutcome:
+        """Checks the outcome of this bet after a given roll.
+
+        Args:
+            roll: Dice roll sum of the current round.
+            point: Point number, or None if not set.
+
+        Returns:
+            The outcome of the bet.
+        """
+        raise NotImplementedError('Must be overridden in a child class')
+
+
+class PassBet(Bet):
+    """A bet on the shooter winning."""
+
+    type = BetType.PASS
+
+    def check(self, *, roll: int, point: Union[int, None]) -> BetOutcome:
+        if point is None:
+            if roll in (7, 11):
+                return BetOutcome.WIN
+            elif roll in (2, 3, 12):
+                return BetOutcome.LOSE
+        else:
+            if roll == point:
+                return BetOutcome.WIN
+            elif roll == 7:
+                return BetOutcome.LOSE
+        return BetOutcome.UNDECIDED
+
+
+class DontPassBet(Bet):
+    """A bet on the shooter losing."""
+
+    type = BetType.DONT_PASS
+
+    def check(self, *, roll: int, point: Union[int, None]) -> BetOutcome:
+        if point is None:
+            if roll in (2, 3):
+                return BetOutcome.WIN
+            elif roll in (7, 11):
+                return BetOutcome.LOSE
+            elif roll == 12:
+                return BetOutcome.TIE
+        else:
+            if roll == 7:
+                return BetOutcome.WIN
+            elif roll == point:
+                return BetOutcome.LOSE
+        return BetOutcome.UNDECIDED
+
+
+class PassOddsBet(Bet):
+    """An Odds bet on a Pass bet winning."""
+
+    type = BetType.PASS_ODDS
+
+    def check(self, *, roll: int, point: Union[int, None]) -> BetOutcome:
+        assert point is not None, 'Point must be set for a Pass Odds bet'
+        if roll == point:
+            return BetOutcome.WIN
+        elif roll == 7:
+            return BetOutcome.LOSE
+        return BetOutcome.UNDECIDED
+
+
+class DontPassOddsBet(Bet):
+    """An Odds bet on a Don't Pass bet winning."""
+
+    type = BetType.DONT_PASS_ODDS
+
+    def check(self, *, roll: int, point: Union[int, None]) -> BetOutcome:
+        assert point is not None, "Point must be set for a Don't Pass Odds bet"
+        if roll == 7:
+            return BetOutcome.WIN
+        elif roll == point:
+            return BetOutcome.LOSE
+        return BetOutcome.UNDECIDED
